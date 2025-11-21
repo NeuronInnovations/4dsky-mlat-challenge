88ab213c9d731d93dff111224e7bc056c441d2c4
zoperado <35506609+zoperado@users.noreply.github.com>
2025-11-21 16:02:39 +0000

    Sensors don't reveal their true location and to make mlat work we correct the location of few test sensors by means of hardcoded overrides.   More overrides to be added as needed

--- main-half-4dsky.go
+++ main-half-4dsky.go
@@ -21,10 +21,12 @@ import (
 
 	neuronsdk "github.com/NeuronInnovations/neuron-go-hedera-sdk" // Import neuronFactory from neuron-go-sdk
 	commonlib "github.com/NeuronInnovations/neuron-go-hedera-sdk/common-lib"
+	keylib "github.com/NeuronInnovations/neuron-go-hedera-sdk/keylib"
 
 	"github.com/hashgraph/hedera-sdk-go/v2"
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/libp2p/go-libp2p/core/protocol"
 )
 
@@ -68,9 +70,51 @@ func uint64FromByte(bytes []byte) uint64 {
 	return binary.BigEndian.Uint64(bytes)
 }
 
+// SensorLocation represents a sensor's location
+type SensorLocation struct {
+	Lat float64
+	Lon float64
+	Alt float64
+}
+
+// locationOverridesData contains public keys and their corresponding locations
+var locationOverridesData = []struct {
+	PubKey string
+	Loc    SensorLocation
+}{
+	{"027c4a53123b00b54cd004c684a6f93e02fc423d080dc065c2d2efa9b94b660826", SensorLocation{50.102509, -5.68199, 192.8}},
+	{"0237ec14e56be526c011c6e6f3c23afead31fceabe2302822985a3c187fc6b72b8", SensorLocation{49.95967, -6.28422, 67.0}},
+	{"025d2f109933ce63bb998bc3cf41549e1557fcd1937c5349a9f6f586226ad72fc3", SensorLocation{49.94661, -6.33147, 52.4}},
+	{"02e2b54578b8861b5e57aee78bd2452a00d73d30d7706b35856af8b5d56f74d1df", SensorLocation{49.89252, -6.34473, 74.7}},
+	{"0359507a76239e5b7db5bd20b900225d72cdc6be4e11599e3e0b97c5c815a75d1a", SensorLocation{49.91428, -6.29487, 91.4}},
+	{"02388bdffad38859b6892baaa511a174fcb1dd84f3cb74cc8feb088e004ce5ddf4", SensorLocation{50.10246, -5.66815, 185.8}},
+	{"03f34b08f08471eb8049739d2e9c7fa09326e871a648d0b067bc5f517046dd4f15", SensorLocation{50.15498, -5.65337, 180.8}},
+	{"021a29e755951b0f0267c0357e8d01489ae41abbe54818e4975b33ca5553b7a902", SensorLocation{50.09917, -5.55674, 146.4}},
+	{"022b58a706abfd29d12011f7e08ad037aac80a9c3651ccd5d44679cfb6bfcd409a", SensorLocation{50.20571, -5.49855, 149.4}},
+}
+
+// initLocationOverrides converts the hardcoded data to a map of peer ID to location
+func initLocationOverrides() map[peer.ID]SensorLocation {
+	overrides := make(map[peer.ID]SensorLocation)
+	for _, data := range locationOverridesData {
+		peerIDStr := keylib.ConvertHederaPublicKeyToPeerID(data.PubKey)
+		peerID, err := peer.Decode(peerIDStr)
+		if err != nil {
+			log.Printf("Warning: failed to decode peer ID %s: %v", peerIDStr, err)
+			continue
+		}
+		overrides[peerID] = data.Loc
+	}
+	return overrides
+}
+
 func main() {
 	//"neuron/ADSB/0.0.2"
 	var NrnProtocol = protocol.ID("neuron/ADSB/0.0.2")
+
+	// Initialize location overrides
+	locationOverrides := initLocationOverrides()
+	log.Printf("Initialized %d location overrides", len(locationOverrides))
 
 	neuronsdk.LaunchSDK(
 		"0.1",       // Specify your app's version
@@ -153,6 +197,14 @@ func main() {
 
 					// (7) rawModeS (remaining bytes)
 					rawModeS := packetBytes[offset:]
+
+					// Apply location override if available
+					if override, exists := locationOverrides[peerID]; exists {
+						sensorLatitude = override.Lat
+						sensorLongitude = override.Lon
+						sensorAltitude = override.Alt
+						fmt.Printf("*** Applied location override for peer %s ***\n", peerID)
+					}
 
 					// Print the parsed information
 					fmt.Printf("=== ModeS Message from Peer %s ===\n", peerID)
